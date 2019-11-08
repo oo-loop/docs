5b82b9c2aaa4e3efd4527ca02f68af5c82dd1d0e
Michael Vurchio <michael.vurchio@gmail.com>
2019-11-09 00:39:42 +0900

    start documentation

--- src/routes/docs/index.svelte
+++ src/routes/docs/index.svelte
@@ -5,6 +5,15 @@
 <style>
 </style>
 
-<HeadTitle title="Documentation"/>
+<HeadTitle title="Getting Started"/>
 
-<h1>Documentation</h1>
+<h1>Start with Loop</h1>
+<p>Loop offers basic styling by default along with common components and utilities to bootstrap any project.</p>
+<p>Import <strong>Loop framework</strong> into your sass file and <strong>launch it</strong> to start with.</p>
+
+<pre><code>
+<span class="color-secondary">1</span>  <span class="color-pink">@import</span> <span class="color-orange">'~loop/scss'</span>; <span class="color-secondary">// Import loop</span>
+<span class="color-secondary">2</span>  <span class="color-pink">@include</span> <span class="color-yellow">ooCreate</span>; <span class="color-secondary">// Launch loop</span>
+</code></pre>
+
+<h2 class="mt-30">Adjust your own need</h2>
