55ec134efc1e033eaf442b869fd161c0bff91cd8
Michael Vurchio <michael.vurchio@gmail.com>
2019-11-12 15:41:25 +0900

    Simplify Col component

--- src/components/Loop/Col.svelte
+++ src/components/Loop/Col.svelte
@@ -1,19 +1,7 @@
 <script>
   export let prop = '';
-  export let article = false;
-  export let aside = false;
 </script>
 
-{#if article}
-  <article data-oo-col="{prop}">
-    <slot></slot>
-  </article>
-{:else if aside}
-  <aside data-oo-col="{prop}">
-    <slot></slot>
-  </aside>
-{:else}
-  <div data-oo-col="{prop}">
-    <slot></slot>
-  </div>
-{/if}
+<div data-oo-col="{prop}">
+  <slot></slot>
+</div>
