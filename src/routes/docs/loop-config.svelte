972f1e119835b441ca1f4e149b3b0a5176414a37
Michael Vurchio <michael.vurchio@gmail.com>
2019-11-18 18:14:51 +0900

    add colors

--- src/routes/docs/loop-config.svelte
+++ src/routes/docs/loop-config.svelte
@@ -3,15 +3,6 @@
   import highlight from '../../utils/highlight.js';
 </script>
 
-<style>
-  .section {
-    padding: 15px 25px;
-    margin-bottom: 30px;
-    background-color: #fff;
-    box-shadow: 1px 1px 2px rgba(0,0,0, .15);
-  }
-</style>
-
 <HeadTitle title="Config map"/>
 
 <h1>Loop Config Map</h1>
@@ -19,23 +10,32 @@
 
 <p>The single map is there to simplify the use of variables and the configuration of the framework.</p>
 
-<p>Every data is being set inside the variable <code class="inline color-primary">$ooLoop</code></p>
+<p>Every data is being set <strong>inside</strong> the variable <code class="inline color-primary">$ooLoop</code></p>
 {@html highlight(`$ooLoop: (
   // properties
 );
-`, 'scss')}
+`, 'scss', true)}
 
-<h2 class="mt-30" id="global">Global Properties</h2>
+<h2 class="mt-45" id="global">Global Properties</h2>
 <p>The global properties are data to be refered to within your confifuration to avoid duplicating them.</p>
 
-
 <h3 class="h4 mt-30 font-code">breakpoints</h3>
-<p><em class="text-uppercase text-small">MAP</em> - </p>
+<p><em class="text-uppercase text-small">Map</em> - Available breakpoints in your project.</p>
 {@html highlight(`breakpoints: (
   xs: 30em,     // 480px
   sm: 37.500em, // 600px
   md: 60em,     // 960px
   lg: 80em,     // 1280px
   xl: 120em,    // 1980px
-)
-`, 'scss', false)}
+)`, 'scss')}
+
+<h3 class="h4 mt-30 font-code">screens</h3>
+<p><em class="text-uppercase text-small">List</em> - Set of responsive screens being globally used.</p>
+{@html highlight(`screens: (sm, md, lg)`, 'scss')}
+
+<h3 class="h4 mt-30 font-code">colors</h3>
+<p><em class="text-uppercase text-small">Map</em> - Available colors in your project.</p>
+{@html highlight(`colors: (
+  'primary': #0ea7d6,
+  'secondary': #959595,
+)`, 'scss')}
