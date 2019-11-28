266a262d447e42de95889d6ed3fd1f0ac2811e97
Michael Vurchio <michael.vurchio@gmail.com>
2019-11-28 18:01:28 +0900

    finish writing component page

--- src/routes/docs/components.svelte
+++ src/routes/docs/components.svelte
@@ -9,15 +9,21 @@
 <style>
   .notification {
     padding: 1rem;
-    text-align: center;
+    border-left-width: 5px;
+    border-left-style: solid;
     box-shadow: 4px 4px 8px rgba(0, 0, 0, .15);
-    border: 1px solid;
   }
 
   .notification-success {
-    background-color: #c8f7e1;
     color: #015a31;
     border-color: #83cca9;
+    background-color: #c8f7e1;
+  }
+
+  .notification-danger {
+    color: #5a0101;
+    border-color: #ca7878;
+    background-color: #f7c8c8;
   }
 
   .notification-large {
@@ -25,18 +31,12 @@
     font-size: 1.5rem;
   }
 
-  .notification-accent-top {
-    border-width: 0;
+  .notification-centered {
+    text-align: center;
+    border-left: 0;
     border-top-width: 5px;
     border-top-style: solid;
   }
-  .notification-accent-left {
-    border-width: 0;
-    border-left-width: 5px;
-    border-left-style: solid;
-    text-align: left;
-  }
-
 </style>
 
 <HeadTitle title="Components"/>
@@ -78,47 +78,177 @@ You could turn off the html5 validity and have a shorter name for speed purposes
 <p>Loop gives you the possibilty to create your own Loop syntax component.
 That offers you an alternative to regular css in the case of making a recurring style along with numerous modifiers.</p>
 
-<h4 class="mt-30">Example of situation</h4>
+<h4 class="mt-30">Example of a situation</h4>
 <p class="font-italic">I created a reusable component for all the diverse messages of my project.
 I named it <code>.notification</code> and namespaced each modifiers with it.
 I'm happy as it is extremely understandable.</p>
 
 {@html highlight(
 `<!-- Notification component -->
-<div class="notification notification-success notification-large notification-accent-left">
+<div class="notification notification-success notification-large notification-centered">
   <strong>Hey, well done!</strong><br>You've created a successfull notification component
 </div>
 `, 'html')}
 
-<p class="font-italic mt-30">It's a bit redundant. So I refactored the modifiers' logic by using keywords such as <code>is</code> and <code>has</code>.
-I'm very happy as it is still extremely understandable with less writting.</p>
+<p class="font-italic mt-30">It's a bit redundant. So I refactored the modifiers' logic by using keywords such as <code>is</code> which I am using for utilities as well.
+I'm very happy as it is still extremely understandable with less writing.</p>
 
 {@html highlight(
 `<!-- Notification component with shorter modifier -->
-<div class="notification is-success is-large has-accent-left">
+<div class="notification is-success is-large is-centered">
   <strong>Hey, well done!</strong><br>You've created a successfull notification component
 </div>
 `, 'html')}
 
-<p class="font-italic mt-30">While adding utilities in some cases, the class attribute gets crowded, making it harder to know what's going on at a glance.
-I sometimes get confused to differentiate the modifiers from the utilities.</p>
+<p class="font-italic mt-30">In the case where I'm adding utilities, the class attribute gets crowded, making it harder to know what's going on at a glance.
+I sometimes get confused between the modifiers and the utilities.</p>
 {@html highlight(
 `<!-- Notification component along with utilities -->
-<div class="notification is-success is-large has-accent-left font-italic float-left hidden@print mt-15 mb-30">
+<div class="notification is-success is-large is-centered has-font-italic is-float-left is-hidden-print">
   <strong>Hey, well done!</strong><br>You've created a successfull notification component
 </div>
 `, 'html')}
 
 <p class="font-italic mt-30">I refactored my component again using Loop syntax. I'm extremely happy with the separation of concerns as I know what does what as well as avoiding redundant text.</p>
 {@html highlight(
-`<!-- Loop Notification component with utilities -->
-<div oo-notification="success large accent-left" class="font-italic float-left hidden@print mt-15 mb-30">
+`<!-- Loop Notification component with Loop utilities -->
+<div oo-notification="success large centered" class="font-italic float-left hidden@print">
   <strong>Hey, well done!</strong><br>You've created a successfull notification component
 </div>
 `, 'html')}
 
-<h4 class="mt-30">Use of mixin</h4>
+<h3 class="mt-45">Component creation</h3>
+<p>Combine the two mixins <code>ooComponent()</code> and <code>ooComponentVariant()</code> and develop your own Loop component <strong>after the instantiation of the Loop config</strong>.</p>
 
-<div class="notification notification-success notification-large notification-accent-top mb-30">
-  <strong>Hey, well done!</strong><br>You've created a successfull notification component.
+<h4 class="font-code mt-30">ooComponent(<span class="color-primary">$name</span>){'{ '}<span class="color-secondary">@content</span>{' }'}</h4>
+<p><em class="text-uppercase text-small">Mixin</em> - Create a base component</p>
+<ul>
+  <li><strong>$name</strong> <em class="font-monospace">(string)</em> The name of the component</li>
+  <li><strong>@content</strong> CSS rules</li>
+</ul>
+{@html highlight(
+`/* From class Notification component */
+.navigation {
+  padding: 1rem;
+  border-left-width: 5px;
+  border-left-style: solid;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, .15);
+}
+
+/* To Loop Notification component */
+@include ooComponent('navigation') {
+  padding: 1rem;
+  border-left-width: 5px;
+  border-left-style: solid;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, .15);
+}
+`, 'css')}
+
+<h4 class="font-code mt-45">ooComponentVariant(<span class="color-primary">$componentName</span>, <span class="color-primary">$variantName</span>){'{ '}<span class="color-secondary">@content</span>{' }'}</h4>
+<p><em class="text-uppercase text-small">Mixin</em> - Create a component variation</p>
+<ul>
+  <li><strong>$componentName</strong> <em class="font-monospace">(string)</em> The name of the component</li>
+  <li><strong>$variantName</strong> <em class="font-monospace">(string)</em> The name of the variant</li>
+  <li><strong>@content</strong> CSS rules</li>
+</ul>
+
+{@html highlight(
+`/* From class Notification modifiers */
+.notification-success {
+  color: #015a31;
+  border-color: #83cca9;
+  background-color: #c8f7e1;
+}
+.notification-danger {
+  color: #5a0101;
+  border-color: #ca7878;
+  background-color: #f7c8c8;
+}
+.notification-large {
+  padding: 1.75rem 2rem;
+  font-size: 1.5rem;
+}
+.notification-centered {
+  text-align: center;
+  border-left: 0;
+  border-top-width: 5px;
+  border-top-style: solid;
+}
+
+/* To Loop Notification modifiers */
+@include ooComponentVariant('navigation', 'success') {
+  color: #015a31;
+  border-color: #83cca9;
+  background-color: #c8f7e1;
+}
+@include ooComponentVariant('navigation', 'danger') {
+  color: #5a0101;
+  border-color: #ca7878;
+  background-color: #f7c8c8;
+}
+@include ooComponentVariant('navigation', 'large') {
+  padding: 1.75rem 2rem;
+  font-size: 1.5rem;
+}
+@include ooComponentVariant('navigation', 'centered') {
+  text-align: center;
+  border-left: 0;
+  border-top-width: 5px;
+  border-top-style: solid;
+}
+`, 'css')}
+
+
+{@html highlight(
+`<div oo-notification="success large centered">
+  <strong>Hey, well done!</strong><br>You've created a <i>success</i> notification component
+</div>`, 'html')}
+<div class="notification notification-success notification-large notification-centered mb-30">
+  <strong>Hey, well done!</strong><br>You've created a <i>success</i> notification component.
 </div>
+
+{@html highlight(
+`<div oo-notification="danger">
+  <strong>Oops!</strong><br>You've created a <i>danger</i> notification component.
+</div>`, 'html')}
+<div class="notification notification-danger mb-30">
+  <strong>Oops!</strong><br>You've created a <i>danger</i> notification component.
+</div>
+<div class="hr"></div>
+<h4>Create from config</h4>
+<p>It is possible to create components from the config file at the condition of CSS selectors being at one level (not nested).</p>
+
+{@html highlight(
+`$ooLoop: ooAdd('components', (
+  'notification': (
+    props: (
+      padding: 1rem,
+      border-left-width: 5px,
+      border-left-style: solid,
+      box-shadow: 4px 4px 8px rgba(0, 0, 0, .15),
+    ),
+    variants: (
+      'success': (
+        color: #015a31,
+        border-color: #83cca9,
+        background-color: #c8f7e1,
+      )
+      'danger': (
+        color: #5a0101,
+        border-color: #ca7878,
+        background-color: #f7c8c8,
+      ),
+      'large': (
+        padding: 1.75rem 2rem,
+        font-size: 1.5rem,
+      ),
+      'centered': (
+        text-align: center,
+        border-left: 0,
+        border-top-width: 5px,
+        border-top-style: solid,
+      )
+    )
+  ),
+));
+`, 'scss')}
