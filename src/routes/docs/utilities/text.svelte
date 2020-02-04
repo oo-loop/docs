a8e7896cb82f28a0fe8a221b7d16c512c6155365
Michael Vurchio <michael.vurchio@gmail.com>
2020-02-04 23:33:24 +0900

    add update section on text utitlites

--- src/routes/docs/utilities/text.svelte
+++ src/routes/docs/utilities/text.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <style>
-
+ .text-large-2 {
+  font-size: rem(18);
+ }
 </style>
 
 <HeadTitle title="Text utilities"/>
@@ -63,22 +65,35 @@ text: (
 <h4>Add more or update data <span class="font-regular text-medium">(<a href="docs/utilities#format" title="Check the options">Check the options</a>)</span></h4>
 
 {@html highlight(
-`$ooLoop: ooPipe(
-  _add('palette', (
-    'danger': #da3b3b // danger globally
+`$ooLoop: ooAdd('text', (
+  fontSize: ('values': (
+    'tiny': rem(12), // add .text-tiny
+    'large': rem(18), // update .text-large
   )),
-  _add('color.background.values', (
-    'danger': #f7c8c8, //overwrite the danger value for background
+  fontWeight: ('values': (
+    'light': 300,  // add .font-light
   )),
-  _add('color.border.values', (
-    _this('palette.danger'), //add global danger to border values
+  fontFamily: ('values': (
+    'monospace': monospace, // add .font-monospace
+    'serif': (Georgia, "Times New Roman", Times), //add .font-serif
   )),
-);
+  letterSpacing: (
+    prefix: 'text',
+    values: (
+      'tight': -1px, // add .text-tight
+      'wide': 1.5px,  // add .text-wide
+    )
+  )
+));
 `, 'scss')}
 
 <div class="ground">
-  <p class="color-danger mb-0">This is a text with the class <em>.color-danger</em></p>
-  <div class="hr"></div>
-  <p class="wrapper-small bg-danger">this is a block with a the class <em>.bg-danger</em></p>
-  <p class="wrapper-small border-danger" style="border: 1px solid">this is a block with a the class <em>.border-danger</em></p>
+  <p class="text-tiny mb-0">This is a text with the class <em>.text-tiny</em></p>
+  <p class="text-large-2">This is a text with the updated size of the class <em>.text-large</em></p>
+  <!-- for documentation purposes the class used is .text-large-2 -->
+  <p class="font-light">This is a text with the class <em>.font-light</em></p>
+  <p class="font-monospace mb-0">This is a text with the class <em>.font-monospace</em></p>
+  <p class="font-serif">This is a text with the class <em>.font-serif</em></p>
+  <p class="text-tight mb-0">This is a text with the class <em>.text-tight</em></p>
+  <p class="text-wide mb-0">This is a text with the class <em>.text-wide</em></p>
 </div>
