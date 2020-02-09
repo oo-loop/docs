48974a3afb0a3e3f3a570b922eee07d0839ceaa4
Michael Vurchio <michael.vurchio@gmail.com>
2020-02-09 20:59:39 +0900

    add wrapper doc

--- src/routes/docs/utilities/wrapper.svelte
+++ src/routes/docs/utilities/wrapper.svelte
@@ -3,12 +3,80 @@
   import highlight from '@/utils/highlight.js';
 </script>
 
-<style>
-
+<style lang="scss">
+.cell {
+  background-color: oo('palette.sample.dark');
+}
+[class^="wrapper"] {
+  background-color: oo('palette.sample.light');
+}
 </style>
 
 <HeadTitle title="Wrapper utilities"/>
 
 <h1>Wrapper utilities</h1>
-<p class="text-large">A bunch of handy utility classes to customize elements on the fly.</p>
-<p>Loop provides recurring utilities to start with.</p>
+<p class="text-large">Wrap elements with padding around them.</p>
+<p>Two classes available from start <code>.wrapper-small</code> and <code>.wrapper-medium</code>.</p>
+<p class="info">In manual mode add the mixin <strong>Wrapper()</strong></p>
+
+{@html highlight(
+`// default config
+wrapper: (
+  property: 'padding',
+  values: (
+    'small': 1rem,
+    'medium': 1.5rem,
+  ),
+),
+`, 'scss')}
+{@html highlight(
+`<div class="wrapper-small">small</div>
+<div class="wrapper-medium">medium</div>
+`, 'html')}
+<div class="ground text-center">
+  <div class="wrapper-small mb-15">
+    <div class="cell">small</div>
+  </div>
+  <div class="wrapper-medium">
+    <div class="cell">medium</div>
+  </div>
+</div>
+
+<h4 class="mt-30">Add more, update data, set responsive values <span class="font-regular text-medium">(<a href="docs/utilities#format" title="Check the options">Check the options</a>)</span></h4>
+{@html highlight(
+`$ooLoop: ooAdd('wrapper.values', (
+  'tiny': .5rem,
+  'wide': (
+    rt: 1rem,
+    sm: 2rem,
+    md: 3rem,
+    lg: 4rem,
+  )
+));
+
+// set .wrapper-tiny@sm .wrapper-small@sm .wrapper-medium@sm
+// set .wrapper-tiny@md .wrapper-small@md
+// set .wrapper-small@lg .wrapper-medium@lg
+$ooLoop: ooSet('wrapper.screens', (
+  sm: (),
+  md: ('tiny', 'small'),
+  lg: ('small', 'medium')
+));
+`, 'scss')}
+
+{@html highlight(
+`<div class="wrapper-tiny">tiny</div>
+<div class="wrapper-wide">responsive</div>
+<div class="wrapper-tiny wrapper-small@sm wrapper-medium@lg">With breakpoints</div>
+`, 'html')}
+<div class="ground text-center">
+  <div class="wrapper-tiny mb-15">
+    <div class="cell">tiny</div>
+  </div>
+  <div class="wrapper-tiny wrapper-small@sm wrapper-medium@lg mb-15">
+    <div class="cell">With breakpoints</div>
+  </div>
+  <div class="wrapper-wide">
+    <div class="cell">responsive</div>
+  </div>
+</div>
